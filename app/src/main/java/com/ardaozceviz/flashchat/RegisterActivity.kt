54339ab898ea024c1d4578cc6cfa4ae76df445aa
Arda Ozceviz <ardaozceviz@hotmail.com>
2017-11-01 20:40:55 +0300

    firebase user registration inital implementation

--- app/src/main/java/com/ardaozceviz/flashchat/RegisterActivity.kt
+++ app/src/main/java/com/ardaozceviz/flashchat/RegisterActivity.kt
@@ -5,13 +5,21 @@ import android.support.v7.app.AppCompatActivity
 import android.text.TextUtils
 import android.util.Log
 import android.view.View
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
 
+    lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        firebaseAuth = FirebaseAuth.getInstance()
     }
 
     // Executed when Sign Up button is pressed.
@@ -34,13 +42,20 @@ class RegisterActivity : AppCompatActivity() {
         var cancel = false
         var focusView: View? = null
 
+      /*  // Check if password is empty
+        if (TextUtils.isEmpty(password)){
+            register_password.error = getString(R.string.error_field_required)
+            focusView = register_password
+            cancel = true
+        }*/
+
         // Check for a valid password, if the user entered one.
-        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
+        if (TextUtils.isEmpty(password) || !isPasswordValid(password)) {
+            Log.d("RegisterActivity", "Password Check")
             register_password.error = getString(R.string.error_invalid_password)
             focusView = register_password
             cancel = true
         }
-
         // Check for a valid email address.
         if (TextUtils.isEmpty(email)) {
             register_email.error = getString(R.string.error_field_required)
@@ -51,14 +66,12 @@ class RegisterActivity : AppCompatActivity() {
             focusView = register_email
             cancel = true
         }
-
         if (cancel) {
             // There was an error; don't attempt login and focus the first
             // form field with an error.
             focusView!!.requestFocus()
         } else {
-            // TODO: Call create FirebaseUser() here
-
+            createFirebaseUser()
         }
     }
 
@@ -69,6 +82,20 @@ class RegisterActivity : AppCompatActivity() {
 
     fun isPasswordValid(password: String): Boolean {
         Log.d("RegisterActivity", "isPasswordValid() executed")
-        return true
+        val confirmPassword = register_confirm_password.text.toString()
+        val result: Boolean
+        return confirmPassword == password && password.length >4
+    }
+
+    fun createFirebaseUser(){
+        val email = register_email.text.toString()
+        val password = register_password.text.toString()
+        firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener({task: Task<AuthResult> ->
+            Log.d("RegisterActivity", "createFirebaseUser(): ${task.isSuccessful}")
+            Log.d("RegisterActivity", "createFirebaseUser(): $task.")
+            if (!task.isSuccessful) {
+                Log.d("RegisterActivity", "createFirebaseUser() is failed: ${task.exception}")
+            }
+        })
     }
 }
